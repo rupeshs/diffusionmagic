5c0db98d636d67317a344c9f6470cc6088844ba4
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2023-02-26 21:19:27 +0530

    Added install script linux

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
-#Part of DiffusionMagic - Script to install DiffusionMagic
-BASEDIR=$(dirname "$0")
-echo "$BASEDIR"
-$BASEDIRtools/linux/micromamba -r $BASEDIRenv create -y -f $BASEDIRenvironment.yml
-pause
+#!/bin/bash
+# Part of DiffusionMagic - Script to install DiffusionMagic
+BASEDIR=$(pwd)
+chmod +x $BASEDIR/tools/linux/micromamba
+chmod +x start.sh
+$BASEDIR/tools/linux/micromamba -r $BASEDIR/env create -y -f $BASEDIR/environment.yml
+$BASEDIR/tools/linux/micromamba shell init -s bash -p $BASEDIR/umamba
+read -n1 -r -p "Press any key to continue..." key
