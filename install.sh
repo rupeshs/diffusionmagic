b3d634deec0ca81da281fdd382f12081e930add4
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2023-02-27 18:19:19 +0530

    Removed settings.yaml, updated linux install and start scripts

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ chmod +x $BASEDIR/tools/linux/micromamba
 chmod +x start.sh
 $BASEDIR/tools/linux/micromamba -r $BASEDIR/env create -y -f $BASEDIR/environment.yml
 $BASEDIR/tools/linux/micromamba shell init -s bash -p $BASEDIR/umamba
-read -n1 -r -p "Press any key to continue..." key
+read -n1 -r -p "Diffusion magic installation completed,press any key to continue..." key
--- start.sh
+++ start.sh
@@ -3,6 +3,4 @@
 echo Starting DiffusionMagic please wait...
 BASEDIR=$(pwd)
 eval "$(micromamba shell hook --shell=bash)"
-micromamba activate
-#$BASEDIR/tools/Linux/micromamba activate $BASEDIR/envs/diffusionmagic-env && python $BASEDIR/src/app.py
-
+micromamba activate $BASEDIR/env/envs/diffusionmagic-env && python $BASEDIR/src/app.py
