dea2cc1d2e8bbe2c581e1a5d8899092ff995e43c
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2023-03-05 15:52:58 +0530

    Added shareable link

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
-from frontend.web.ui import diffusionmagic_web_ui
-from settings import AppSettings
+import argparse
+
 from backend.computing import Computing
 from backend.generate import Generate
+from frontend.web.ui import diffusionmagic_web_ui
+from settings import AppSettings
 
 # mypy --ignore-missing-imports --explicit-package-bases .
 # flake8 --max-line-length=100 .
@@ -13,7 +15,15 @@ if __name__ == "__main__":
         print(f"ERROR in loading application settings {ex}")
         print("Exiting...")
         exit()
+    parser = argparse.ArgumentParser(description="DiffusionMagic")
+    parser.add_argument(
+        "-s", "--share", help="Shareable link", required=False, default=False
+    )
+    args = parser.parse_args()
     compute = Computing()
     generate = Generate(compute)
     dm_web_ui = diffusionmagic_web_ui(generate)
-    dm_web_ui.launch()
+    if args.share:
+        dm_web_ui.launch(share=True)
+    else:
+        dm_web_ui.launch()
