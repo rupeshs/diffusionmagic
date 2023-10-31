4c17fcc2047f9012db8efa9ab7645f22cb2d1121
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2023-10-31 18:10:14 +0530

    Removed micromamba mac/micromamba

--- install-mac.sh
+++ install-mac.sh
@@ -1,8 +1,30 @@
-#!/bin/bash
-# Part of DiffusionMagic - Script to install DiffusionMagic
+#!/usr/bin/env bash
+echo Starting DiffusionMagic env installation...
+set -e
+PYTHON_COMMAND="python3"
+
+if ! command -v python3 &>/dev/null; then
+    if ! command -v python &>/dev/null; then
+        echo "Error: Python not found, please install python 3.8 or higher and try again"
+        exit 1
+    fi
+fi
+
+if command -v python &>/dev/null; then
+   PYTHON_COMMAND="python"
+fi
+
+echo "Found $PYTHON_COMMAND command"
+
+python_version=$($PYTHON_COMMAND --version 2>&1 | awk '{print $2}')  
+echo "Python version : $python_version"
+
 BASEDIR=$(pwd)
-chmod +x $BASEDIR/tools/mac/micromamba
-chmod +x start.sh
-$BASEDIR/tools/mac/micromamba -r $BASEDIR/env create -y -f $BASEDIR/environment.yml
-$BASEDIR/tools/mac/micromamba shell init -s bash -p $BASEDIR/umamba
-read -n1 -r -p "Diffusion magic installation completed,press any key to continue..." key
+
+$PYTHON_COMMAND -m venv "$BASEDIR/env"
+# shellcheck disable=SC1091
+source "$BASEDIR/env/bin/activate"
+pip install torch==2.0.1 torchvision=0.15.2
+pip install -r "$BASEDIR/requirements.txt"
+chmod +x "start.sh"
+read -n1 -r -p "DiffusionMagic installation completed,press any key to continue..." key
--- start.sh
+++ start.sh
@@ -1,6 +1,25 @@
-#!/bin/bash
-# Part of DiffusionMagic - Script to Start DiffusionMagic
+#!/usr/bin/env bash
 echo Starting DiffusionMagic please wait...
+set -e
+PYTHON_COMMAND="python3"
+
+if ! command -v python3 &>/dev/null; then
+    if ! command -v python &>/dev/null; then
+        echo "Error: Python not found, please install python 3.8 or higher and try again"
+        exit 1
+    fi
+fi
+
+if command -v python &>/dev/null; then
+   PYTHON_COMMAND="python"
+fi
+
+echo "Found $PYTHON_COMMAND command"
+
+python_version=$($PYTHON_COMMAND --version 2>&1 | awk '{print $2}')  
+echo "Python version : $python_version"
+
 BASEDIR=$(pwd)
-eval "$(micromamba shell hook --shell=bash)"
-micromamba activate $BASEDIR/env/envs/diffusionmagic-env && python $BASEDIR/src/app.py
+# shellcheck disable=SC1091
+source "$BASEDIR/env/bin/activate"
+$PYTHON_COMMAND src/app.py 
